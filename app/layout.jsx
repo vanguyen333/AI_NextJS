052956d5e19b3ecef579478d92dc3e312deb2118
vanguyen333 <vanguyen333@yahoo.com>
2023-11-02 22:19:31 -0400

    added page and layout files in App folder, added background

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "descover & Share AI Prompts",
+};
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
