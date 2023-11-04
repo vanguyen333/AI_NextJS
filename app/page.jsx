7f4d94901907e0e0ecbb92d2a9953e1111ff9f5b
vanguyen333 <vanguyen333@yahoo.com>
2023-11-03 22:05:54 -0400

    imported Nav and Feed to layout

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Feed from "@components/Feed";
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
@@ -10,6 +11,7 @@ const Home = () => {
         discover, create and share creative prompts
       </p>
       {/* Feed */}
+      <Feed />
     </section>
   );
 };
