effb3e29022d5d477b486822b86df4e77fa307ad
vanguyen333 <vanguyen333@yahoo.com>
2023-11-06 22:09:10 -0500

    created database file using mongoose

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,14 +1,21 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers";
-
+console.log({
+  clientId: process.env.GOOGLE_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+});
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: "",
-      clientSecret: "",
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   async session({ session }) {},
-  async signIn({ profile }) {},
+  async signIn({ profile }) {
+    try {
+      //serverless -> Lambda
+    } catch (error) {}
+  },
 });
 export { handler as GET, handler as POST };
