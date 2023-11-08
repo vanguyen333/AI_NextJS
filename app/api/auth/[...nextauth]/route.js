7e6d52c306fb11bd00d9cd356241326c50504a55
vanguyen333 <vanguyen333@yahoo.com>
2023-11-07 22:42:51 -0500

    config database successfully

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,8 @@
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { User } from "@models/user";
 import { connectToDB } from "@utils/database";
-import { connect } from "mongoose";
-import { User } from "next-auth";
+
 console.log({
   clientId: process.env.GOOGLE_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -14,13 +14,29 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+    session.user.id = sessionUser._id.toString();
+    return session;
+  },
   async signIn({ profile }) {
     try {
       //serverless -> Lambda
       await connectToDB();
       //check if a user already exists
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
       //if not create a new user, save to DB
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
       return true;
     } catch (error) {
       console.log(error);
--- models/user.js
+++ models/user.js
@@ -18,6 +18,11 @@ const UserSchema = new Schema({
     type: String,
   },
 });
+//The 'models' object is provided by the MOngoose Library and stores all the reistered models.
+//if a model named "User" already exists in the 'models' object, it assigns that existing model to the 'User' variable.
+//This prevents redefining the model and ensures that the existing model is reused.
+//If a model named "User" does not exist in the "models" object, the "model" funciton from Mongoose is called to create a new model
+//The newly created model is then assigned to the "User" variable.
 
 const User = models.User || model("User", UserSchema);
 
