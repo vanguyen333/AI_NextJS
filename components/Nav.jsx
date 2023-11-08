14cd7c5e7f1bfde9e8b72104cb670a0ea10aaff5
vanguyen333 <vanguyen333@yahoo.com>
2023-11-08 16:16:26 -0500

    user is able to sign in

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,15 +6,16 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 import { set } from "mongoose";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
+
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDrowdown] = useState(false);
   useEffect(() => {
-    const setPRoviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
       setProviders(response);
     };
-    setProviders();
+    setUpProviders();
   }, []);
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -29,9 +30,11 @@ const Nav = () => {
         />
         <p className="logo_text">Promptopia</p>
       </Link>
+      {/* {alert(session?.user)} */}
+      {/* {alert(providers)} */}
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -67,7 +70,7 @@ const Nav = () => {
       </div>
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
               src="/assets/images/logo.svg"
