ca33d7bf053296abdda0bc470bdc7e4aaccc8d6f
vanguyen333 <vanguyen333@yahoo.com>
2023-11-08 22:37:00 -0500

    added styled h1 to create post Form

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import Link from "next/link";
 
 const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
-  return <div>Form</div>;
+  return (
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient"> {type} Post</span>
+      </h1>
+      <p></p>
+    </section>
+  );
 };
 
 export default Form;
