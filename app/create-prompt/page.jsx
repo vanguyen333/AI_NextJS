1e5658265eba8393067f3a969879af9a8552cc71
vanguyen333 <vanguyen333@yahoo.com>
2023-11-08 22:33:18 -0500

    added basic useState hooks to page.jsx for the Form file

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,5 +1,27 @@
+"use client";
+
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Form from "@components/Form";
+
 const CreatePrompt = () => {
-  return <div>CreatePrompt</div>;
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+  const CreatePrompt = async (e) => {};
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={CreatePrompt}
+    />
+  );
 };
 
 export default CreatePrompt;
