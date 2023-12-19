57b2680f8dcdbd8a9c3884291391ce769b0e4de9
vanguyen333 <vanguyen333@yahoo.com>
2023-12-18 22:28:01 -0500

    added styles to create btn

--- components/Form.jsx
+++ components/Form.jsx
@@ -45,7 +45,11 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
           <Link href="/" className="text-gray-500 text-sm">
             Cancel
           </Link>
-          <button type="submit" disabled={submitting}>
+          <button
+            type="submit"
+            disabled={submitting}
+            className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white"
+          >
             {submitting ? `${type}...` : type}
           </button>
         </div>
