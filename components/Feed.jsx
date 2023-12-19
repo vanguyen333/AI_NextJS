b1042afa25d78a50bc6d047db7e8c622f5893f11
vanguyen333 <vanguyen333@yahoo.com>
2023-12-19 16:52:26 -0500

    added search input box to home page

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
+
+import PromptCard from "./PromptCard";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+  const handleSearchChange = (e) => {};
+  return (
+    <section className="feed">
+      {" "}
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+    </section>
+  );
 };
 
 export default Feed;
